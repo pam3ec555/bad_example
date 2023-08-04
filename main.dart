af29e740cc1ee8787314a1f541da5b362a8eb238
Ramil <rep555333@gmail.com>
2023-08-04 17:58:34 +0700

    Update main.dart

--- main.dart
+++ main.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 void main() {
-  runApp(BadStatefulApp());
+  runApp(BadStatefulApp("Some title"));
 }
 
 class BadStatefulApp extends StatelessWidget {
